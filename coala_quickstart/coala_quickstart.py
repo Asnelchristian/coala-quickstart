5245542ffeb551ec4d6b75a8517e2568e0b5efb0
Adhityaa C <c.adhityaa@gmail.com>
2016-05-27 18:49:57 +0530

    Suggest bears based on project, write to coafile

--- coala_quickstart/coala_quickstart.py
+++ coala_quickstart/coala_quickstart.py
@@ -1,35 +1,75 @@
-#!/usr/bin/env python3
+import os
 
-from os.path import abspath, join
+from pyprint.ConsolePrinter import ConsolePrinter
 
-
-def get_project_dir():
-    """
-    Retrieves the project directory from the user:
-
-    >>> from coalib.misc.ContextManagers import simulate_console_inputs
-    >>> from coalib.misc.ContextManagers import suppress_stdout
-    >>> with simulate_console_inputs("/abs/path"), suppress_stdout():
-    ...     dir = get_project_dir()
-    >>> assert dir == "/abs/path"
-
-    If he doesn't feel like giving a directory, we'll assume the current
-    directory:
-
-    >>> with simulate_console_inputs(""), suppress_stdout():
-    ...     dir = get_project_dir()
-    >>> assert dir == abspath(".")
-    """
-    dir = input(
-        "Hey fellow! Awesome you decided to do high quality coding. "
-        "We hope we can help you with that. Let's get started, what is your "
-        "project directory? We'll create a `.coafile` in there with some "
-        "settings. Is the current directory fine? (press enter to confirm or "
-        "enter the project directory) ")
-    return abspath(dir)
+from coalib.output.printers.LogPrinter import LogPrinter
+from coalib.collecting.Collectors import collect_files
+from coala_quickstart.messages import (
+    print_welcome_message, give_info)
+from coala_quickstart.General import (
+    get_project_dir, get_sections, get_file_globs, get_bears,
+    give_bear_help, get_section_bears)
+from coala_quickstart.Utilities import get_popular_languages
+from coala_quickstart.ConfWriter import write_coafile
 
 
 def main():
-    dir = get_project_dir()
-    files_glob = input("Please enter the files glob: ")
-    coafile = open(join(dir, ".coafile"), "a+")
+    printer = ConsolePrinter()
+    log_printer = LogPrinter(printer)
+
+    print_welcome_message(printer)
+
+    project_dir = get_project_dir(printer)
+
+    file_globs = get_file_globs(
+        printer,
+        msg="Which files do you want to lint?",
+        help_text=True)
+    ignore_file_globs = get_file_globs(
+        printer,
+        msg="Which files do you want to ignore?",
+        help_text=False)
+
+    path_globs = []
+    ignore_globs = []
+    for file_glob in file_globs:
+        path_globs.append(os.path.join(project_dir, file_glob))
+    for ignore_file_glob in ignore_file_globs:
+        ignore_globs.append(os.path.join(project_dir, ignore_file_glob))
+
+    file_paths = collect_files(
+        path_globs,
+        log_printer,
+        ignored_file_paths=ignore_globs)
+    give_info(printer, str(len(file_paths)) + " file(s) matched!")
+
+    results = get_popular_languages(file_paths)
+    if len(results):
+        print("\nThe following languages have been automatically detected:")
+        for lang, percent in results:
+            printer.print(
+                "\t" + lang + " (" + str(percent) + "%)", color="cyan")
+        print("")
+    bears = get_bears(log_printer)
+    give_bear_help(printer, results, bears)
+    print("")
+
+    sections = ["Default"] + get_sections(printer)
+    filtered_sections = []
+    for section in sections:
+        if section not in filtered_sections:
+            filtered_sections.append(section)
+
+    bear_pre_text = False
+    settings = {}
+    for section in filtered_sections:
+        printer.print("[" + section + "]", color="green")
+        selected_bears = get_section_bears(printer, bear_pre_text, bears)
+        settings[section] = {"bears": selected_bears}
+
+    write_coafile(project_dir, settings)
+
+    give_info(
+        printer,
+        "That's it! Go to your project directory and run `coala` "
+        "for awesome automatic code analysis!")
