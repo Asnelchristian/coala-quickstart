9a208ec2a388778016a3bd685d179bc3907118f4
Adhityaa C <c.adhityaa@gmail.com>
2016-06-05 14:04:26 +0530

    generation: Identify most used languages
    Add methods to automatically identify the most used
    languages in the user's project based on file extensions.

--- coala_quickstart/generation/Project.py
+++ coala_quickstart/generation/Project.py
@@ -1,4 +1,5 @@
 import os
+import operator
 
 from coala_quickstart.interaction.Question import ask_question
 from coala_quickstart.Extensions import exts
@@ -25,3 +26,66 @@ def get_project_dir(printer):
             printer.print("Please enter a valid directory", color="blue")
 
     return project_dir
+
+
+def get_used_languages(file_paths):
+    """
+    Identifies the most used languages in the user's project directory
+    from the files matched from the given glob expression.
+
+    :param file_paths:
+        A list of absolute file paths in the user's project directory.
+    :return:
+        A list of tuples containing a language name as the first value
+        and percentage usage in the project as the second value.
+    """
+    results = {lang: 0 for ext in exts for lang in exts[ext]}
+    results["Unknown"] = 0
+
+    for file_path in file_paths:
+        ext = os.path.splitext(file_path)[1]
+        if ext in exts:
+            for lang in exts[ext]:
+                results[lang] += 1
+        else:
+            results["Unknown"] += 1
+
+    if file_paths:
+        for language in results:
+            results[language] = (100 * results[language]) / len(file_paths)
+
+    languages = sorted(
+        results.items(),
+        key=operator.itemgetter(1),
+        reverse=True)
+    append_list = []
+    unknown_percent = 0
+    for lang, percent in languages:  # pragma: no branch
+        if percent > 0:
+            if lang == "Unknown":
+                unknown_percent = percent
+            else:
+                yield (lang, percent)
+
+    if unknown_percent > 0:
+        yield ("Unknown", unknown_percent)
+
+
+def print_used_languages(printer, results):
+    """
+    Prints the sorted list of used languages along with each language's
+    percentage use.
+
+    :param printer:
+        A ``ConsolePrinter`` object used for console interactions.
+    :param results:
+        A list of tuples containing a language name as the first value
+        and percentage usage in the project as the second value.
+    """
+    if results:
+        printer.print(
+            "The following languages have been automatically detected:")
+        for lang, percent in results:
+            formatted_line = "{:>25}: {:>2}%".format(lang, int(percent))
+            printer.print(formatted_line, color="cyan")
+        printer.print()
--- tests/generation/Project.py
+++ tests/generation/Project.py
@@ -3,8 +3,9 @@ import unittest
 
 from pyprint.ConsolePrinter import ConsolePrinter
 from coalib.misc.ContextManagers import (
-    simulate_console_inputs, suppress_stdout)
-from coala_quickstart.generation.Project import get_project_dir
+    simulate_console_inputs, suppress_stdout, retrieve_stdout)
+from coala_quickstart.generation.Project import (
+    get_project_dir, get_used_languages, print_used_languages)
 
 
 class TestQuestion(unittest.TestCase):
@@ -26,3 +27,43 @@ class TestQuestion(unittest.TestCase):
         with simulate_console_inputs("/invaliddir", "/tmp"), suppress_stdout():
             project_dir = get_project_dir(self.printer)
             self.assertEqual(project_dir, "/tmp")
+
+class TestPopularLanguages(unittest.TestCase):
+
+    def setUp(self):
+        self.printer = ConsolePrinter()
+
+    def test_get_used_languages(self):
+        file_lists = [["/tmp/file.py", "/tmp/file.py"],
+            ["/tmp/file.py", "/tmp/test.cpp"],
+            ["/tmp/file.py"],
+            ["/tmp/file.py", "/tmp/unknown.extension"],
+            ["/tmp/unknown.extension"],
+            []]
+        results = [[('Python', 100)],
+            [('Python', 50), ('C++', 50)],
+            [('Python', 100)],
+            [('Python', 50), ('Unknown', 50)],
+            [('Unknown', 100)],
+            []]
+
+        for file_list, expected_result in zip(file_lists, results):
+            result = get_used_languages(file_list)
+            self.assertEqual(sorted(result), sorted(expected_result))
+
+    def test_print_used_languages(self):
+        with retrieve_stdout() as custom_stdout:
+            print_used_languages(self.printer, [('Python', 100)])
+            res = custom_stdout.getvalue()
+            self.assertIn("Python", res)
+            self.assertIn("100%", res)
+            self.assertNotIn("Unknown", res)
+
+        with retrieve_stdout() as custom_stdout:
+            print_used_languages(self.printer, [('Python', 75), ('C++', 25)])
+            self.assertIn("75%\n", custom_stdout.getvalue())
+
+    def test_no_results(self):
+        with retrieve_stdout() as custom_stdout:
+            print_used_languages(self.printer, [])
+            self.assertNotIn("following langauges", custom_stdout.getvalue())
